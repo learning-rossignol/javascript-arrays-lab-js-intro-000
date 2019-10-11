2c4649f80c5132b99e5352d75b71522365dab71a
Jean-François Rompré <jrompre@gmail.com>
2019-10-11 17:37:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyPrependKitten(name) {
 }
 
 function destructivelyRemoveLastKitten() {
-  kittens.pop;
+  kittens.pop();
   return kittens;
 }
 
@@ -28,7 +28,7 @@ function prependKitten(name){
 }
 
 function removeLastKitten(){
-  return kittens.slice(0, kittens.length);
+  return kittens.slice(0, kittens.length - 1);
 }
 
 function removeFirstKitten(){
